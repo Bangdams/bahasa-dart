e20a9276a281c81f02f6acb7b618dc442ada5fce
Bangdams <sadamjr67@gmail.com>
2025-04-16 23:46:45 +0700

    menambahkan folder belajar dart yang didalamnya terdapat import ke dalam dart library

---
+++ belajar_dart_application/bin/belajar_dart_application.dart
@@ -0,0 +1,11 @@
+// bisa gini
+// import 'package:belajar_dart_library/hello.dart';
+
+// atau pakai as
+import 'package:belajar_dart_library/hello.dart' as belajar;
+
+void main() {
+  print(belajar.Product());
+  print(belajar.Category());
+  print(belajar.sayHello('sadam'));
+}
